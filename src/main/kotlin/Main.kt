3ca68f0de57bc4428f6b94807483ae1a3b987825
SuRakib <rakibul15-10976@diu.edu.bd>
2023-03-26 20:02:32 +0600

    Initial Commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,38 @@
+fun main() {
+
+    var age=90
+
+    when(age){
+
+        20->{
+             println("It is 20")
+        }
+        30->{
+            println("It is 30")
+        }
+        40->{
+            println("It is 40")
+        }
+        50->{
+            println("It is 50")
+        }
+        60->{
+            println("It is 60")
+        }
+
+        else->{
+            println("Invalid Number")
+
+        }
+
+
+
+
+    }
+
+
+
+
+
+
+}
