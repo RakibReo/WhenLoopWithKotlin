23beb61093d810e37fe7a4ca54f92fba4a037b8b
SuRakib <rakibul15-10976@diu.edu.bd>
2023-04-05 22:21:12 +0600

    Second Commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,34 +1,45 @@
 fun main() {
+     val num=10
+    var age=100
 
-    var age=90
-
-    when(age){
-
-        20->{
-             println("It is 20")
-        }
-        30->{
-            println("It is 30")
-        }
-        40->{
-            println("It is 40")
-        }
-        50->{
-            println("It is 50")
-        }
-        60->{
-            println("It is 60")
-        }
-
-        else->{
-            println("Invalid Number")
-
-        }
+     if (age<num){
 
 
+         when(age){
+
+             20->{
+                 println("It is 20")
+             }
+             30->{
+                 println("It is 30")
+             }
+             40->{
+                 println("It is 40")
+             }
+             50->{
+                 println("It is 50")
+             }
+             60->{
+                 println("It is 60")
+             }
+
+             else->{
+                 println("Invalid Number")
+
+             }
 
 
-    }
+
+
+         }
+
+
+
+     }else{
+
+         print("Invalid Numbeer: $num")
+
+     }
 
 
 
